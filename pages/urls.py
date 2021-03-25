98aaf11bfa7c0a2832b895c0f08b0941f81fb177
Chuck.py <vynny_marcus@hotmail.com>
2021-03-25 18:27:45 -0300

    app pages app

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import AboutPageView, HomePageView
+
+app_name = "pages"
+
+urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
+]
--- pycom/settings.py
+++ pycom/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'pycom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = "users.User"
 
