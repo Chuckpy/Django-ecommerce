81b1e926100a85ed13c1994ac7b119a920e1f251
Chuck.py <vynny_marcus@hotmail.com>
2021-04-05 18:39:34 -0300

    paralax att

--- static/js/darktheme.js
+++ static/js/darktheme.js
@@ -3,3 +3,13 @@ const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 });
+
+function save() {	
+	var checkbox = document.getElementById("chk");
+    localStorage.setItem("chk", checkbox.checked);	
+}
+
+//for loading
+var checked = JSON.parse(localStorage.getItem("chk"));
+    document.getElementById("chk").checked = checked;
+
